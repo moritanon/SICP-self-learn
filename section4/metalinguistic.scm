ef4f8a4ca242eea980df3e7f07567901cb416a50
moritanon <asnnote@yahoo.co.jp>
2010-06-11 21:54:51 +0900

    2nd test

---
+++ section4/metalinguistic.scm
@@ -0,0 +1,494 @@
+
+#|
+基本式
+数値のような自己評価式
+evalは値を得るため、環境から変数を探すひつようがある。
+
+
+特殊形式
+
+／／／
+
+|#
+
+(define (eval exp env)
+  (cond ((self-evaluating? exp) exp)
+    ((varable? exp) (lookup-variable-value exp env))
+    ((quoted? exp) (text-of-quotation exp))
+    ((assignment? exp) (eval-assignment exp env))
+    ((definition? exp) (eval-definition exp env))
+    ((if? exp) (eval-if exp env))
+    ((lambda? exp)
+     (make-procedure (lambda-parameters exp)
+             (lambda-body exp)
+             env))
+    ((begin? exp)
+     (eval-sequence (begin-actions exp) env))
+    ((cond? exp) (eval (cond->if exp) env))
+    ((application? exp)
+     (apply (eval (operator exp) env)
+        (list-of-values (operands exp) env)))
+    (else
+     (error "Unknown expression type -- EVAL" exp))))
+
+;; apply
+
+(define (apply procedure arguments)
+  (cond ((primitive-procedure? procedure)
+     (apply-primitive-procedure procedure argments))
+    ((compound-procedure? procedure)
+     (eval-sequence
+      (procedure-body procedure)
+      (extend-environment
+       (procedure-parameters procedure)
+       arguments
+       (procedure-environment procedure))))
+    (else
+     (error
+      "Unknown procedure type -- APPLY" procedure))))
+
+;; 手続きの引数
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+      '()
+      (cons (eval (first-operand exps) env)
+        (list-of-values (rest-operands exps) env))))
+;;条件式
+(define (eval-if exp env)
+  (if (true? (eval (if-predicate exp) env))
+      (eval (if-consequent exp) env)
+      (eval (if-alternative exp) env)))
+
+;;並び
+(define (eval-sequence exps env)
+  (cond ((last-exp? exps) (eval (first-exp exps) env))
+    (else (eval (first-exp exps) env)
+          (eval-sequence (rest-exps exps) env))))
+
+
+;;代入と定義
+(define (eval-assignment exp env)
+  (set-variable-value! (assignment-variable exp)
+               (eval (assignment-value exp) env)
+               env)
+  'ok)
+
+(define (eval-difinition exp env)
+  (define-variable! (definition-variable exp)
+                    (eval (definition-value exp) env)
+            env)
+  'ok)
+
+;;4.1
+(define (list-of-values-lr exps env)
+  (if (no-operands? exps)
+      '()
+      (let ((evaled-exp (eval (first-operand exps) env)))
+	(cons evaled-exp
+           (list-of-values-lr (rest-operands exps) env)))))
+
+(define (list-of-values-lr exps env)
+  (if (no-operands? exps)
+      '()
+      (let ((evaled-rest (list-of-values-lr (rest-operands exps) env)))
+    (cons (eval (first-operand exps) env)
+          evaled-rest))))
+
+;; 式の表現
+;; 自己評価式 数値と文字列
+(define (self-evaluating? exp)
+  (cond ((number? exp) #t)
+    ((string? exp) #t)
+    (else #f)))
+;; 変数
+(define (variable? exp) (symbol? exp))
+;; クォート式
+(define (quoted? exp)
+    (tagged-list? exp 'quote))
+
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+      (eq? (car exp) tag)
+      #f))
+
+(define (text-of-quotation exp)
+  (cadr exp))
+
+;;代入
+(define (assignment? exp)
+  (tagged-list? exp 'set!))
+
+(define (assignment-variable exp) (cadr exp))
+(define (assignment-value exp) (caddr exp))
+
+;;定義
+(define (definition? exp)
+  (tagged-list? exp 'define))
+
+(define (definition-variable exp)
+  (if (symbol? (cadr exp))
+      (cadr exp)
+      (caddr exp)))
+
+(define (definition-value exp)
+  (if (symbol? (cadr exp))
+      (caddr exp)
+      (make-lambda (cdadr exp)
+           (cddr exp))))
+
+
+
+(define (make-definition variable value)
+  (list 'define variable value))
+
+
+
+
+;; lambda 式
+(define (lambda? exp)
+  (tagged-list? exp 'lambda))
+(define (lambda-parameters exp)
+  (cadr exp))
+(define (lambda-body exp)
+  (cddr exp))
+
+(define (make-lambda parameters body)
+  (cons 'lambda (cons parameters body)))
+
+;;条件式
+(define (if? exp)
+  (tagged-list? exp 'if))
+
+(define (if-predicate exp)
+  (cadr exp))
+
+(define (if-consequent exp)
+  (caddr exp))
+
+(define (if-alternative exp) 
+  (if (not (null? (cadddr exp)))
+      (cadddr exp)
+      'false))
+      
+    ;; cond->if で使用
+(define (make-if predicate consequent alternative)
+  (list 'if predicate consequent alternative))
+
+;; begin
+(define (begin? exp) (tagged-list? exp 'begin))
+
+(define (begin-actions exp) (cdr exp))
+
+(define (last-exp? seq) (null? (cdr seq)))
+
+(define (first-exp seq) (car seq))
+
+(define (rest-exp seq) (cdr seq))
+
+(define (sequence->exp seq)
+  (cond ((null? seq) seq)
+	((last-exp? seq) (first-exp seq))
+	(else
+	 (make-begin seq))))
+
+(define (make-begin seq) (cons 'begin seq))
+
+;; 手続き作用
+(define (application? exp) (pair? exp))
+
+(define (operator exp) (car exp))
+
+(define (operands exp) (cdr exp))
+
+(define (no-operands? ops) (null? ops))
+
+(define (first-operand ops) (car ops))
+
+(define (rest-operand ops) (cdr ops))
+
+
+;; cond
+(define (cond? exp) (tagged-list? exp 'cond))
+(define (cond-clauses exp) (cdr exp))
+(define (cond-else-clause? clause)
+  (eq? (cond-predicate clause) 'else))
+(define (cond-predicate clause) (car clause))
+(define (cond-actions clause) (cdr clause))
+(define (cond->if exp)
+  (expand-clauses (cond-clauses exp)))
+
+(define (expand-clauses clauses)
+  (if (null? clauses)
+      'false
+      (let ((first (car clauses))
+	    (rest (cdr clauses)))
+	(if (cond-else-clause? first)
+	    (if (null? rest)
+		(sequence->exp (cond-actions first))
+		(error "ELSE clause is'nt last --COND->IF" clauses))
+	    (make-if (cond-predicate first)
+		     (sequence->exp (cond-actions first))
+		     (expand-clauses rest))))))
+(define (cond=>? acts)
+  (tagged-list? acts '=>))
+(define (cond-recievers acts)
+  (cadr acts))
+(define (cond-send-reciever predicate act)
+  (list act predicate))
+
+
+(define *talbe* (make-hash-table 'equal?))
+
+(define (get package key)
+  (hash-table-get *table* (list package key) #f))
+(define (put package key value)
+  (hash-table-put! *table* (list package key) value))
+
+
+
+
+(define (eval exp env)
+  (cond ((self-evaluating? exp) exp)
+	((varable? exp) (lookup-variable-value exp env))
+	(else
+	 (let ((op (get 'eval (operator exp))))
+	   (cond ((op) (op exp env))
+		 ((application? exp)
+		  (apply (eval op env)
+			 (list-of-values (operands exp) env)))
+		 (else
+		  (error "Unknown expression type -- EVAL" exp)))))))
+
+
+;; 4.4 and or
+;; (and '()) と (and) は区別されなければならない。
+;; というより自動的に区別される
+
+(define (and? exp) (tagged-list? exp 'and))
+(define (or? exp)  (tagged-list? exp 'or))
+(define (and-operands exp) (cdr exp))
+(define (or-operands exp) (cdr exp))
+
+(define (eval-and-seq seq env)
+  (if (null? seq)     
+      'true
+      (let ((evaled-value (eval (first-exp seq) env)))
+    (cond ((false? evaled-value) 'false)
+          ((last-exp? seq) evaled-value)
+          (else
+           (eval-and-seq (rest-exp seq) env))))))
+
+(define (eval-and exp env)
+  (eval-and-seq (and-operands exp) env))
+
+(define (eval-or-seq seq env)
+  (if (null? seq)
+      'false
+      (let ((evaled-value (eval (first-exp seq) env)))
+    (cond ((true? evaled-value) evaled-value)
+          (else
+           (eval-and-seq (rest-exp seq) env))))))
+
+
+
+(define (eval-or exp env)
+  (define (rest-seq-or seq) (cdr seq))
+  (define (first-seq-or seq) (car seq))
+  (define (eval-or-seq seq env)
+    (if  (null? seq) 'false
+     (let ((evaled-or (eval (first-seq-or seq))))
+       (cond ((true? evaled-or) evaled-or)
+         (else 
+          (eval-or-seq (rest-seq-or seq) env)))))))
+
+(define (eval-or exp env)  (eval-or-seq (or-operands exp) env))
+
+;; let
+(define (let? exp)
+  (tagged-list? exp 'let))
+
+(define (let-params exp)
+  (cadr exp))
+
+(define (let-body exp)
+  (caddr exp))
+
+(define (first-let-param params)
+  (car params))
+(define (rest-let-param params)
+  (cdr params))
+
+(define (let-param-variable param)
+  (car param))
+(define (let-param-value param)
+  (cadr param))
+#|
+(define (let-params->var-list params)
+  (if (null? params) '()
+      (cons (let-param-variable (first-let-param params))
+	    (let-params->var-list (rest-let-param params)))))
+(define (let-params->val-list params)
+  (if (null? params) '()
+      (cons (let-param-value (first-let-param params))
+	    (let-params->var-list (rest-let-param params)))))
+|#
+(define (let-params->var-val-list params)
+  (if (null? params)
+      (values '() '())
+      (receive (vars vals) (let-params->var-val-list (cdr params))
+	       (values (cons (let-param-variable 
+			      (first-let-param params))
+			     vars)
+		       (cons (let-param-value 
+			      (first-let-param params))
+			     vals)))))
+	 
+
+(define (let->combination exp)
+  (let ((body (let-body exp)))
+    (receive (vars vals)
+	     (let-params->var-val-list (let-params exp))
+	     (if (null? vals)
+		 (list (make-lambda vars body))
+		 (cons (make-lambda vars body) vals)))))
+
+(define (make-let var val body)
+  (list 'let (list var val) body))
+;;4.7
+;;let*->nested-lets
+(define (let*? exp) (tagged-list? exp 'let*))
+
+(define (let*->nested-lets exp)
+  (define (let*-iter params body)
+    (if (null? params) body
+	(list 'let 
+	      (list (car params))
+	      (let*-iter (cdr params) body))))
+  (let*-iter (let-params exp) (let-body exp)))
+
+
+
+(define (install-eval-package)
+  ;;手続きの定義は上のほうですんでることにする。 
+  ;; interfaceが違うものだけなんとかする。
+  (define (eval-lambda exp env)
+    (make-procedure (lambda-parameters exp)
+            (lambda-body exp) env))
+  (define (eval-begin exp env)
+    (eval-sequence (begin-actions exp) env))
+
+  (define (eval-cond exp env)
+    (eval (cond->if exp) env))
+  (define (eval-let exp env)
+    (eval (let->combination exp) env))
+  (define (eval-let* exp env)
+    (eval (let*->nested-lets exp) env))
+  (put 'eval 'quote text-of-quotation)
+  (put 'eval 'set! eval-assignment)
+  (put 'eval 'define eval-difinition)
+  (put 'eval 'if eval-if)
+  (put 'eval 'lambda  eval-lambda)
+  (put 'eval 'begin eval-begin)
+  (put 'eval 'cond eval-cond)
+  (put 'eval 'or eval-or)
+  (put 'eval 'and eval-and)
+)
+
+;; 4-1-3
+(define (false? x)
+  (eq? x #f))
+(define (true? x) 
+  (not (false? x)))
+
+;; 仮定
+;; (apply-primitive-procedure <proc> <args>)
+;; (primitive-procedure? <proc>)
+
+(define (make-procedure parameters body env)
+  (list 'procedure parameters body env))
+
+(define (compound-procedure? p)
+  (tagged-list? p 'procedure))
+
+(define (procedure-parameter p)
+  (cadr p))
+
+(define (procedure-body p)
+  (caddr p))
+
+(define (procedure-environment p)
+  (cadddr p))
+
+;; 環境
+;; (lookup-variable-value <var> <env>)
+;; (extend-environment <variables> <values> <base-env>)
+;; (define-variable! <var> <value> <env>)
+;; (set-variable-value! <var> <value> <env>)
+
+
+(define (enclosing-environment env) (cdr env))
+(define (first-frame env) (car env))
+(define the-empty-environment '())
+
+;; frame var & val
+
+(define (make-frame variables values)
+  (cons variables values))
+
+(define (frame-variables frame) (car frame))
+(define (frame-values frame) (cdr frame))
+
+(define (add-binding-frame! var val frame)
+  (set-car! frame (cons var (frame-variables frame)))
+  (set-cdr! frame (cons val (frame-values frame))))
+	    
+(define (extend-environment vars vals base-env)
+  (if (= (length vars) (length vals))
+      (cons (make-frame vars vals) base-env)
+      (if (< (length vars) (length vals))
+	  (error "Too many argments supplied" var vals)
+	  (error "Too few argments supplied" var vals))))
+
+(define (lookup-variable-value var env)
+  (define (env-loop env)
+    (define (scan vars vals)
+      (cond ((null? vars) 
+	     (env-loop (enclosing-environment env)))
+;;	     (enclosing-environment env))
+	    ((eq? var (car vars)) (car vals))
+	    (else
+	     (scan (cdr vars) (cdr vals)))))
+    (if (eq? env the-empty-environment)
+	(error "Unbound variable" var)
+	(let ((frame (first-frame env)))
+	  (scan (frame-variables frame)
+		(frame-values frame)))))
+  (env-loop env))
+		 
+(define (set-variable-value! var val env)
+  (define (env-loop env)
+    (define (scan vars vals)
+      (cond ((null? vars)
+	     (env-loop (enclosing-environment env)))
+	    ((eq? var (car vars))
+	     (set-car! vals val))
+	    (else
+	     (scan (cdr vars) (cdr vals)))))
+    (if (eq? env the-empty-environment)
+	(error "Unbound variable -- SET!" var)
+	(let ((frame (first-frame env)))
+	  (scan (frame-variables frame)
+		(frame-values frame)))))
+  (env-loop env))
+
+(define (define-variable! var val env)
+  (let ((frame (first-frame env)))
+    (define (scan vars vals)
+      (cond ((null? vars)
+	     (add-binding-frame! var vals frame))
+	    ((eq? var (car vars))
+	     (set-car! vals val))
+	    (else
+	     (scan (cdr vars) (cdr vals)))))
+    (scan (frame-variables frame)
+	  (frame-values frame))))
---
+++ section4/test-metalingu.scm
@@ -0,0 +1,244 @@
+(use gauche.test)
+(test-start "metalinuistic.scm")
+
+(require "./metalinguistic.scm")
+;;(load "./metalinguistic.scm")
+
+(test* "eval-1" 1 (eval '1 '()))
+(test* "self-evaluating1" #t (self-evaluating? 1))
+(test* "self-evaluating2" #t (self-evaluating? "abc"))
+(test* "self-evaluating3" #f (self-evaluating? '()))
+(test* "self-evaluating4" #f (self-evaluating? 'a))
+
+(test* "variable? 1"  #t (variable? 'a))
+(test* "variable? 2" #f (variable? 2))
+
+(test* "tagged-list? 1" #t (tagged-list? '(lambda (a b) (a b)) 'lambda))
+(test* "tagged-list? 2" #f (tagged-list? 123 'abc))
+(test* "quoted? 1" #f (quoted? '(a b c)))
+(test* "quoted? 2" #t (quoted? '(quote a b c)))
+
+(test* "text-of-quotation 1" '(a b c) (text-of-quotation '(quote (a b c))))
+
+(test* "assignment? 1" #t (assignment? '(set! a 10)))
+(test* "assignment? 2" #f (assignment? '(a b c)))
+
+(test* "assignment-variable 1" 'a (assignment-variable '(set! a 10)))
+(test* "assignment-value 1" 10 (assignment-value '(set! a 10)))
+
+(test* "definition? 1" #t (definition? '(define a 10)))
+(test* "definition? 2" #f (definition? '(a b c)))
+
+(test* "definition-variable 1" 'a (definition-variable '(define a 10)))
+(test* "definition-value 1" 10 (definition-value '(define a 10)))
+
+(test* "make-definitaion 1" '(define a 10) (make-definition 'a 10))
+
+(test* "lambda? 1" #t (lambda? '(lambda (a b c) (+ a b c))))
+(test* "lambda-parameters 1" '(a b c) (lambda-parameters '(lambda (a b c) (+ a b c))))
+(test* "lambda-body 1" '((+ a b c)) 
+       (lambda-body '(lambda (a b c) (+ a b c))))
+(test* "lambda-body 2" '(1) (lambda-body '(lambda (a b c) 1)))
+(test* "make-lambda 1" '(lambda (a b c) (+ a b c))
+       (make-lambda '(a b c) '((+ a b c))))
+(test* "make-lambda 2" '(lambda (a b c) . 1)
+       (make-lambda '(a b c) 1))
+
+(test* "if? 1" #t (if? '(if a b c)))
+(test* "if-predicate 1" 'a (if-predicate '(if  a b c)))
+(test* "if-consequent 1" 'b (if-consequent '(if a b c)))
+(test* "if-alternative 1" 'c (if-alternative '(if a b c)))
+
+(test* "make-if 1" '(if a b c) (make-if 'a 'b 'c))
+
+(test* "begin? exp 1" #t (begin? '(begin a b)))
+(test* "begin-actions 1" '(a b) (begin-actions '(begin a b)))
+(test* "last-exp? seq" #f (last-exp? '(a b)))
+(test* "last-exp? seq" #t (last-exp? '(a)))
+
+(test* "first-exp 1" 'a (first-exp '(a b)))
+
+(test* "rest-exp 1" '(b) (rest-exp '(a b)))
+
+(test* "sequence->exp 1" '() (sequence->exp '()))
+(test* "sequence->exp 2" 'a (sequence->exp '(a)))
+(test* "sequence->exp 3" '(begin a b) (sequence->exp '(a b)))
+(test* "make-begin 1"  '(begin a b) (make-begin '(a b)))
+
+(test* "application? 1" #t (application? '(a b)))
+(test* "application? 2" #f (application? 1))
+
+
+(test* "operator 1" 'a (operator '(a b)))
+(test* "operands 1" '(b) (operands '(a b)))
+
+(test* "no-operands? 1" #f (no-operands? '(1 2)))
+(test* "no-operands? 2" #t (no-operands? '()))
+
+(test* "first-operand 1" 'a (first-operand '(a b)))
+(test* "rest-operand 1" '(b) (rest-operand '(a b)))
+
+;; cond
+(test* "cond? 1" #t (cond? '(cond ((= a b) #t))))
+(test* "cond? 2" #f (cond? '(if a b c)))
+
+;; (cond (a x)
+;;       (b y)
+;;       (else z))  
+(test* "cond-clauses 1" '((a x) (b y) (else z))
+       (cond-clauses '(cond (a x) (b y) (else z))))
+
+(test* "cond-else-clause? 1" #t (cond-else-clause? '(else z)))
+(test* "cond-else-clause? 2" #f (cond-else-clause? '(a x)))
+
+(test* "cond-predicate 1" 'a (cond-predicate '(a x)))
+(test* "cond-actions 1" '(x) (cond-actions '(a x)))
+
+(test* "cond->if 1" '(if a x y)
+       (cond->if '(cond (a x) (else y))))
+(test* "cond->if 2" '(if a x (if b y z))
+       (cond->if '(cond (a x) (b y) (else z))))
+       
+(test* "expand-clauses 1" '(if a x y)
+       (expand-clauses '((a x) (else y))))
+
+(test* "expand-clauses 2" '(if a x (if b y z))
+       (expand-clauses '((a x) (b y) (else z))))
+
+;;(test-error "expand-clauses 3" (lambda () ((expand-clauses '((a x) (else y) (b z))))))
+;; error関数形式のエラーの補足の仕方  -- 課題
+;;(test* "" (test-error) (load "./nosuchfile"))
+;;(test* "" (test-error <error>) (load "./nosuchfile"))
+
+;; => 以下のように行えばよいらしい
+(test* "expand-clauses 3" (test-error) (expand-clauses '((a x) (else y) (b z))))
+
+(test* "expand-clauses 4" '(if a x false) (expand-clauses '((a x))))
+(test* "expand-clauses 5" '(if a (begin x y) z) 
+       (expand-clauses '((a x y) (else z))))
+
+(test* "and? 1" #t (and? '(and #t)))
+(test* "or? 1" #t (or? '(or 0)))
+
+(test* "and-operands 1" '(a b) (and-operands '(and a b)))
+(test* "or-operands 1" '(a b) (or-operands '(or a b)))
+
+;; let
+(test* "let? 1" #t (let? '(let ((a 1) (b 2)) body)))
+(test* "let-params 1" '((a 1) (b 2)) (let-params '(let ((a 1) (b 2)) body)))
+(test* "let-body 1" 'body (let-body '(let ((a 1) (b 2)) body)))
+(test* "first-let-param 1" '(a 1) (first-let-param '((a 1) (b 2))))
+(test* "rest-let-param 1" '((b 2)) (rest-let-param '((a 1) (b 2))))
+(test* "let-param-variable 1" 'a (let-param-variable '(a 1)))
+(test* "let-param-value 1" 1 (let-param-value '(a 1)))
+
+
+(test* "let-params->var-val-list 1" 
+       (call-with-values (lambda () 
+			   (values '(a) '(1))) list)
+       (call-with-values (lambda () 
+			   (let-params->var-val-list '((a 1)))) list))
+(test* "let-params->var-val-list 1.5"
+       (receive all
+		(values '(a) '(1)) 
+		all)
+       (receive all
+		(let-params->var-val-list '((a 1))) 
+		all))
+		
+
+
+(test* "let-params->var-val-list 1"
+       (values-ref (values '(a) '(2)) 0)
+       
+(test* "let-params->var-val-list 2" 
+       (call-with-values (lambda () (values '(a b) '(1 2))) list)
+       (call-with-values (lambda () (let-params->var-val-list '((a 1) (b 2)))) list))
+
+(test* "let->combination 1" 
+       '((lambda (a) . body) 1)
+       (let->combination '(let ((a 1)) body)))
+(test* "let->combination 2"
+       '((lambda (a b) . body) 1 2)
+       (let->combination '(let ((a 1) (b 2)) body)))
+
+(test* "let->combination 3"
+       '((lambda () . body))
+       (let->combination '(let () body)))
+
+(test* "make-let 1"
+       '(let (a 1) body)
+       (make-let 'a 1 'body))
+
+(test* "let*->nested-lets 1"
+       '(let ((a 1))
+	  (let ((b 1))
+	    body))
+       (let*->nested-lets '(let* ((a 1) (b 1)) body)))
+(test* "let*0>nested-lets 2"
+       '(let ((a 1)) body)
+       (let*->nested-lets '(let* ((a 1)) body))) 
+       
+
+(test* "make-procedure 1" '(procedure (a b c) (+ a b c) env)
+       (make-procedure '(a b c) '(+ a b c) 'env))
+
+(test* "compound-procedure? 1" #t 
+       (compound-procedure? (make-procedure '(a b c) '(+ a b c) 'env)))
+
+
+(test* "make-frame 1" '((a b c) . (1 2 3))
+       (make-frame '(a b c) '(1 2 3)))
+(test* "frame-variables 1" '(a b c)
+       (frame-variables '(a b c) . (1 2 3)))
+
+(test* "frame-values 1" '(1 2 3) 
+       (frame-values (make-frame '(a b c) '(1 2 3))))
+
+(test* "add-binding-frame! 1" '(a b c)
+       (let ((frame (make-frame '(b c) '(1 2))))
+	 (frame-variables (begin 
+			    (add-binding-frame! 'a 1 frame)
+			    frame))))
+						
+(test* "extend-environment 1"
+       '(((a b) . (1 2)) . ((x y) . (0 1)))
+       (extend-environment '(a b) '(1 2) '((x y) . (0 1))))
+			
+(test* "extend-environment 2"
+       (test-error)
+       (extend-environment '(a b c) '(1 2) '()))
+
+(test* "extend-environment 3"
+       (test-error)
+       (extend-environment '(a b) '(1 2 3) '()))
+
+(test* "first-frame 1" '((x y) 0 1)
+       (first-frame 
+	(extend-environment '(x y) '(0 1) the-empty-environment)))
+(test* "first-frame 2" '((a b) 9 10)
+       (first-frame
+	(extend-environment 
+	 '(a b) '(9 10)
+	 (extend-environment '(x y) '(0 1) the-empty-environment))))
+
+(test* "lookup 1" 1
+       (lookup-variable-value 
+	'y 
+	(extend-environment '(x y) '(0 1) the-empty-environment)))
+
+(test* "lookup 2" 0
+       (lookup-variable-value
+	'x
+	(extend-environment 
+	 '(a b) '(9 10)
+	 (extend-environment '(x y) '(0 1) the-empty-environment))))
+
+(test* "lookup 3" (test-error)
+       (lookup-variable-value
+	'p
+	(extend-environment 
+	 '(a b) '(9 10)
+	 (extend-environment '(x y) '(0 1) the-empty-environment))))
+
+(test-end)
